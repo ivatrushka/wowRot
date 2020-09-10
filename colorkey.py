f6959a01d3eb3a7f07ad1c6c3ad891a2f8afcfa9
ivatrushka <temka1234510@yandex.ru>
2020-09-11 03:36:47 +0500

    Add files via upload

---
+++ colorkey.py
@@ -0,0 +1,142 @@
+#Dictionary of keys
+
+GLOBAL_DIST = { 
+        #afliction
+        13329407:{
+        7034623:'-',    # agony
+        16711863:'6',   # instabolt T
+        852223:'0',     # corruption
+        6815576:'5',    # syphon
+        16727414:'8',   # death bolt T
+        1179629:'7',    # soul
+        16740853:'9',   # unstable afliction
+        13619711:'6',   # filler
+        },
+        
+        #demo lock
+        8448:{
+        16718638: '9',   # demonic charge
+        12255007: '6',   # immolate
+        4583935: '7',    # soul strike
+        65280:'-',       # hell hounds
+        16718581:'0',    # hand of guldan
+        8959:'8',        # filler
+        8837119: '5',    # demonic power
+        },
+
+        #prot war
+        16732449:{
+        127231: '6',     # intercept
+        3564799: '-',    # shield slam
+        16752417: '0',   # thunder clap
+        15531979: '9',   # revenge
+        8395263: '8',    # crash
+        8676977: '7',    # shield block
+        4949025: '5',    # ignore pain
+  #        : '', #  
+        },
+
+        #fury war
+        2630911:{
+        5774335: '4',    # intercept
+        16744039: 'l',   # victory rush
+        16187226: '-',   # breacking blockade
+        8837119: '0',    # rampage
+        14352347: '9',   # bladestorm
+        13830655: '8',   # execute
+        7470868: '7',    # bloodrage
+        2097137: '6',    # raginblow
+        16726314: 'k',   # shoulder blow
+        },
+
+        #ench shaman
+        16726016:{
+        16776704: '-',   # stormstrike
+        2825727: '0',    # lavalash
+        5308398: '9',    # rockbite
+        15558655: '8',   # flame buff
+        16763056: '7',   # wolf leap
+        16713593: '6',   # froststrike
+        16726314: 'k',   # crack
+        13830655: 'l',   # earth spike
+        },
+
+        #BM hunter
+        3145616:{
+        5774335: '-',    # kill command
+        16744039: '0',   # bestial wrath
+        16187226: '9',   # murder of crows
+        8837119: '8',    # barbed shot
+        14352347: '7',   # chimera shot
+        13830655: '6',   # cobra shot
+        #7470868: '7',   # -
+        #2097137: '6',   # -
+        16726314: '5',   # aoe shot
+        },
+
+        #brew monk
+        15138614:{
+        5774335: '-',    # knockout
+        16744039: '0',   # keg smash
+        16187226: '9',   # fire breath
+        8837119: '8',    # tiger palm
+        14352347: 'k',   # ironskin brew
+        #13830655: '8',  # -
+        #7470868: '7',   # -
+        #2097137: '6',   # -
+        #16726314: 'k',  # -
+        },
+
+        #WW monk
+        15531868:{
+        #5774335: '4',   # -
+        #16744039: 'l',  # -
+        16187226: '-',   # white tiger strike
+        8837119: '0',    # tiger palm
+        14352347: '9',   # rising sun kick
+        13830655: '8',   # fury fist
+        7470868: '7',    # knockout
+        #2097137: '6',   # -
+        #16726314: 'k',  # -
+        },
+
+        #blood DK
+        16770867:{
+        5774335: '4',    # intercept
+        16744039: 'l',   # victory rush
+        16187226: '-',   # marrowrend
+        8837119: '0',    # death strike
+        14352347: '9',   # blood boil
+        13830655: '8',   # heart strike
+        #7470868: '7',   # -
+        #2097137: '6',   # -
+        #16726314: 'k',  # -
+        },
+
+        #shadow priest
+        5571142:{
+        5774335: '4',    # -
+        16744039: 'l',   # powerword death
+        16187226: '-',   # vompire touch + sw
+        8837119: '0',    # voidrush
+        14352347: '9',   # void arrow
+        13830655: '8',   # mind blast
+        7470868: '7',    # mindflay
+        2097137: '6',    # aoe mindflay
+        16726314: 'k',   # -
+        },
+
+        #монк тт
+        7976742:{
+        5774335: '4',    # -
+        16744039: 'l',   # -
+        16187226: '-',   # tiger paw
+        8837119: '0',    # nockout kick
+        14352347: '9',   # rising sun kick
+        13830655: '8',   # -
+        7470868: '7',    # -
+        2097137: '6',    # -
+        16726314: 'k',   # -
+        }
+        
+}
